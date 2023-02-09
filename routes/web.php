4d0817e1fae66865b6533b4458bbc9400479f39f
Minh Hung <84724817+duongminhhung@users.noreply.github.com>
2023-02-09 14:59:58 +0700

    upload

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,25 @@ Route::patch('update-cart', [CategoryController::class, 'update'])->name('update
 
 
 
+// |--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider within a group which
+// | contains the "web" middleware group. Now create something great!
+// ||--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider within a group which
+// | contains the "web" middleware group. Now create something great!
+// ||--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider within a group which
+// | contains the "web" middleware group. Now create something great!
+// |
